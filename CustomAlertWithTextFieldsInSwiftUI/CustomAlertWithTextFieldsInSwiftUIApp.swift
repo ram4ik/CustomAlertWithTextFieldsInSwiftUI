d5a585279508524ddfdfc036dfe4c24ea5ded6a6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-31 09:10:56 +0300

    Initial Commit

---
+++ CustomAlertWithTextFieldsInSwiftUI/CustomAlertWithTextFieldsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertWithTextFieldsInSwiftUIApp.swift
+//  CustomAlertWithTextFieldsInSwiftUI
+//
+//  Created by ramil on 31.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertWithTextFieldsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
